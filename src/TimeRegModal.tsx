838af8a5730c83e6d23c417ebc8d223db88b4561
Tettamanti Alessandro <alessandro.tettamanti@ti.ch>
2024-09-16 15:54:52 +0200

    feat add delete timelog capability

--- src/TimeRegModal.tsx
+++ src/TimeRegModal.tsx
@@ -1,13 +1,15 @@
 import { useQuery, gql, useMutation } from '@apollo/client';
 import { useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
+import { toHuman } from './utils';
 import ApolloFetchError from './ApolloFetchError';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-export default function TimeRegModal({dayIssues, setDayIssues, userName}) {
+export default function TimeRegModal({dayIssues, setDayIssues, userName, onClose}) {
 
     const handleClose = () => {
         setDayIssues({...dayIssues, show:false});
+        onClose();
     };
 
 
@@ -31,7 +33,7 @@ export default function TimeRegModal({dayIssues, setDayIssues, userName}) {
     }`;
 
 
-   const { loading, error, data, client  } =  useQuery(timelogs, {
+   const { loading, error, data,  refetch  } =  useQuery(timelogs, {
     fetchPolicy: 'network-only',
     variables:{
         startDate: dayIssues.day,
@@ -41,22 +43,44 @@ export default function TimeRegModal({dayIssues, setDayIssues, userName}) {
     });
 
 
+
+        let deleteTimelog  = gql`
+        mutation timelogDelete($id: TimelogID!) {
+            timelogDelete (input: {
+                id: $id
+            }){
+                errors
+            }
+          }
+        `;
+
+    const [deleteTimelogFn, { data: dataMutation , loading: loadingMutation, error: errorMutation }] = useMutation(deleteTimelog, {
+      onCompleted: (data) => {
+       console.log('cancellata');
+       refetch();
+      },
+      onError: (error) => {
+        console.log(error);
+      },
+    });
+
+
+
     const deleteTimeReg = (id) => {
-        console.log("cancello");
+        deleteTimelogFn(  {
+                variables: {
+                  id: id,
+          }, 
+        }         
+      );
     }
-
-
-    useEffect(() => {
-        console.log("render TimeRegModal");         
-        
-    },[])
 
 
 
     return (
         <>
         
-        <Modal show={dayIssues.show} onHide={handleClose} size="lg">
+        <Modal show={dayIssues.show} onHide={handleClose} size="lg" >
           <Modal.Header closeButton>
             <Modal.Title>Modal heading {dayIssues.giId}</Modal.Title>
           </Modal.Header>
@@ -72,13 +96,21 @@ export default function TimeRegModal({dayIssues, setDayIssues, userName}) {
 
             
             <table className="table">
+            <thead>
+              <tr>
+                  <th>Title</th>
+                  <th>Time spent</th>
+                  <th>Action</th>
+              </tr>
 
+            </thead>
             <tbody>
-            {Array.from(data.timelogs.nodes).map((node) => (
+            {Array.from(data.timelogs.nodes).map((node, index) => (
                 
                             node.issue.id == dayIssues.gId ?
-                             <tr key={node.issue.id}>
+                             <tr key={index+"_del"}>
                                 <td>  { node.issue.title }</td>
+                                <td className='text-right'>  { toHuman(node.timeSpent) }</td>
                                 <td>  
                                     <a href='' onClick={(e) => {e.preventDefault(); deleteTimeReg(node.id);}}>Delete</a></td>
 
